087a730a5a6be46ea9e3c19571b8fb3f309c5fee
CALCI-DEV <calci-dev@protonmail.com>
2024-11-12 01:09:58 +0330

    commit command SimpleCalculatur app

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace SimpleCalculatur
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("plaese enter number1 : ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("plaese enter number2 : ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.Write("plaese enter operator(+-*/) : ");
+            string op = Console.ReadLine();
+
+            int result = 0;
+            switch (op)
+            {
+                case "+":
+                    result = num1 + num2;
+                    break;
+                case "-":
+                    result = num1 - num2;
+                    break;
+                case "*":
+                    result = num1 * num2;
+                    break;
+                case "/":
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("plaese enter operator BETWEEN (+-*/)");
+                    break;
+
+
+            }
+            Console.WriteLine($"result : {result}");
+        }
+    }
+}
